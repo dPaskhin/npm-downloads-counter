eea06e10e11bcb111e981e8abaf0aef2a41b61ac
Дмитрий Пасхин <d.pasxin@gmail.com>
2021-05-31 00:40:18 +0300

    refactor: Delete waste try catch

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -3,18 +3,13 @@ import { renderTotalDownloads } from '@src/utils/renderTotalDownloads';
 import { eventHandlers } from '@src/utils/eventHandlers';
 
 const execute = async () => {
-  try {
-    const packageDownloadsByPeriods = await Promise.all(fetchPackageDownloadsByPeriods());
+  const packageDownloadsByPeriods = await Promise.all(fetchPackageDownloadsByPeriods());
 
-    renderTotalDownloads(packageDownloadsByPeriods);
+  renderTotalDownloads(packageDownloadsByPeriods);
 
-    eventHandlers(() => {
-      void execute();
-    });
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error);
-  }
+  eventHandlers(() => {
+    void execute();
+  });
 };
 
 void execute();
